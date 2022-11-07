557d4b76a92ffcddb8e94422f9b72fe17e4d5748
Igor <igor-aptos@users.noreply.github.com>
2022-11-07 14:56:13 -0800

    [forge][try2] Adding latency check for high-gas fee transactions to graceful_overload test
    Simplified #4480 per @sitalkedia's suggestion

--- crates/transaction-emitter-lib/src/emitter/account_minter.rs
+++ crates/transaction-emitter-lib/src/emitter/account_minter.rs
@@ -363,7 +363,7 @@ where
 
     // Wait for source account to exist, this can happen because the corresponding REST endpoint might
     // not be up to date with the latest ledger state and requires some time for syncing.
-    wait_for_single_account_sequence(&client, &source_account, Duration::from_secs(30)).await?;
+    wait_for_single_account_sequence(&client, &source_account, Duration::from_secs(60)).await?;
     while i < num_new_accounts {
         let batch_size = min(max_num_accounts_per_batch, num_new_accounts - i);
         let mut batch = if reuse_account {
@@ -460,7 +460,7 @@ pub async fn execute_and_wait_transactions(
     txns: Vec<SignedTransaction>,
     failure_counter: &AtomicUsize,
 ) -> Result<()> {
-    debug!(
+    info!(
         "[{:?}] Submitting transactions {} - {} for {}",
         client.path_prefix_string(),
         account.sequence_number() - txns.len() as u64,
@@ -583,17 +583,21 @@ pub async fn execute_and_wait_transactions(
     let state = state.into_inner();
 
     for txn in state.txns.iter() {
-        RETRY_POLICY
-            .retry(move || {
-                client.wait_for_transaction_by_hash_bcs(
-                    txn.clone().committed_hash(),
-                    txn.expiration_timestamp_secs(),
-                    Some(Duration::from_secs(120)),
-                    None,
-                )
-            })
+        client
+            .wait_for_transaction_by_hash_bcs(
+                txn.clone().committed_hash(),
+                txn.expiration_timestamp_secs(),
+                Some(Duration::from_secs(120)),
+                None,
+            )
             .await
-            .map_err(|e| format_err!("Failed to wait for transactions: {:?}", e))?;
+            .map_err(|e| {
+                format_err!(
+                    "Failed to wait for transaction: {:?}, gas price: {}",
+                    e,
+                    txn.gas_unit_price()
+                )
+            })?;
     }
 
     debug!(
